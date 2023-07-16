f122ad4e467b91567220c9ff81154ffa016a32e8
meredith233 <mrexgo@gmail.com>
2023-07-16 15:26:05 +0800

    feat

--- 618/dataset.py
+++ 618/dataset.py
@@ -1,10 +1,13 @@
 import os
 import matplotlib.pyplot as plt
+import streamlit as st
+import pandas as pd
 
-x = []
-y = []
+# x = []
+# y = []
 # # 总路径
-path = "../Data/{}/Trajectory"
+path = "D:\JetBrains\PycharmProjects\geoHashImpact\Data\{}\Trajectory"
+dataList = []
 for i in range(182):
     current_path = path.format(str(i).zfill(3))
     plts = os.scandir(current_path)
@@ -13,14 +16,17 @@ for i in range(182):
         with open(path_item, 'r+') as fp:
             for line in fp.readlines()[6::600]:
                 item_list = line.split(',')
-                x.append(float(item_list[0]))
-                y.append(float(item_list[1]))
+                # x.append(float(item_list[0]))
+                # y.append(float(item_list[1]))
+                dataList.append([float(item_list[0]), float(item_list[1])])
+df = pd.DataFrame(dataList, columns=['lat', 'lon'])
+# # 绘制散点图
+# plt.scatter(x, y, 1, c='black')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.title('Scatter Plot')
+#
+# # 显示图像
+# plt.show()
 
-# 绘制散点图
-plt.scatter(x, y, 1, c='black')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Scatter Plot')
-
-# 显示图像
-plt.show()
+st.map(df)
--- 618/dataset1.py
+++ 618/dataset1.py
@@ -1,21 +1,26 @@
 import os
-
+import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
-x = []
-y = []
+# x = []
+# y = []
+dataList = []
 # # 总路径
-path = "../Gowalla_totalCheckins.txt"
+path = "D:\JetBrains\PycharmProjects\geoHashImpact\Gowalla_totalCheckins.txt"
 with open(path, 'r+') as fp:
     for line in fp.readlines()[0::100]:
         item_list = line.split('	')
-        x.append(float(item_list[2]))
-        y.append(float(item_list[3]))
+        # x.append(float(item_list[2]))
+        # y.append(float(item_list[3]))
+        dataList.append([float(item_list[2]), float(item_list[3])])
 
-# 绘制散点图
-plt.scatter(x, y, 1, c='black')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Scatter Plot')
-
-# 显示图像
-plt.show()
+# # 绘制散点图
+# plt.scatter(x, y, 1, c='black')
+# plt.xlabel('x')
+# plt.ylabel('y')
+# plt.title('Scatter Plot')
+#
+# # 显示图像
+# plt.show()
+df = pd.DataFrame(dataList, columns=['lat', 'lon'])
+st.map(df)
