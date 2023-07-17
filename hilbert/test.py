ebdc3ebd03b5f688c7769d5c4b01f747b70ae2a0
meredith233 <mrexgo@gmail.com>
2023-07-18 00:39:17 +0800

    hilbert

--- hilbert/test.py
+++ hilbert/test.py
@@ -1,6 +1,9 @@
+import os
+
 from matplotlib import pyplot as plt, patches
 
 curve = []
+dep = 0
 
 
 def filter_point(x1, x2, y1, y2, points):
@@ -15,6 +18,10 @@ def filter_point(x1, x2, y1, y2, points):
 # 希尔伯特一阶从左下到左上到右上到右下， 编排为 0 -- 3
 # direction 0向上 1向右 2向下 3向左
 def hil(x1, x2, y1, y2, points, direction):
+    global dep
+    dep += 1
+    print(dep)
+    if dep > 400: return
     if direction == 0:
         hil0(x1, x2, y1, y2, points)
     elif direction == 1:
@@ -263,7 +270,20 @@ def hil3(x11, x22, y11, y22, points):
 
 # 加载数据集
 def load():
-    return [[0, 0], [0, 360], [180, 0], [180, 360], [180, 95], [140, 95]]
+    res = []
+    path = "../Data/{}/Trajectory"
+    for i in range(182):
+        current_path = path.format(str(i).zfill(3))
+        plts = os.scandir(current_path)
+        for item in plts:
+            path_item = current_path + "\\" + item.name
+            with open(path_item, 'r+') as fp:
+                for line in fp.readlines()[6::600]:
+                    item_list = line.split(',')
+                    res.append([float(item_list[0]), float(item_list[1])])
+                    break
+            break
+    return res
 
 
 hil(0, 180, 0, 360, load(), 0)
@@ -276,19 +296,18 @@ plt.xlim(0, 180)
 x_list = []
 y_list = []
 for item in curve:
-    rectangle = patches.Rectangle((item['x1'], item['y1']), item['x2'] - item['x1'], item['y2'] - item['y1'], edgecolor='orange', fill=False, linewidth=4)
+    rectangle = patches.Rectangle((item['x1'], item['y1']), item['x2'] - item['x1'], item['y2'] - item['y1'],
+                                  edgecolor='orange', fill=False, linewidth=4)
     ax.add_patch(rectangle)
     rx, ry = rectangle.get_xy()
-    cx = rx + rectangle.get_width()/2.0
-    cy = ry + rectangle.get_height()/2.0
+    cx = rx + rectangle.get_width() / 2.0
+    cy = ry + rectangle.get_height() / 2.0
     x_list.append(cx)
     y_list.append(cy)
     ax.annotate(item['index'], (cx, cy), color='black', weight='bold', fontsize=10, ha='center', va='center')
 
-
-for i in range(len(x_list)-1):
-    dx = x_list[i+1] - x_list[i]
-    dy = y_list[i+1] - y_list[i]
+for i in range(len(x_list) - 1):
+    dx = x_list[i + 1] - x_list[i]
+    dy = y_list[i + 1] - y_list[i]
     plt.quiver(x_list[i], y_list[i], dx, dy, angles='xy', scale=1.03, scale_units='xy', width=0.005)
 plt.show()
-
